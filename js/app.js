9f2fe3b5a19c7553bdbb59c4402fcb6ff7dd8356
TaielNxz <TaielNxz@hotmail.com>
2021-12-10 12:31:28 -0300

    mostrar en consola un objeto con los datos de busqueda

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
 /* =========================================
                 Variables
-========================================= */// Contenedor para los resultados
+========================================= */
+// Campos del buscador
+const marca       = document.querySelector('#marca');
+const year        = document.querySelector('#year');
+const minimo      = document.querySelector('#minimo');
+const maximo      = document.querySelector('#maximo');
+const puertas     = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color       = document.querySelector('#color');
+
+// Contenedor para los resultados
 const resultado   = document.querySelector('#resultado');
+
+// Generar un objeto con la busqueda
+const datosBusqueda = {
+    marca       : '',
+    year        : '',
+    minimo      : '',
+    maximo      : '',
+    puertas     : '',
+    transmision : '',
+    color       : '',
+};
 
 
 /* =========================================
@@ -10,6 +31,58 @@ const resultado   = document.querySelector('#resultado');
 document.addEventListener('DOMContentLoaded', () => {
     // Mostrar autos en el HTML ( la variable autos viene de db.js )
     mostrarAutos(autos);
+
+    // Llenamos el select de 'year'
+    llenarSelectYear();
+});
+
+marca.addEventListener('change', e => {
+    // Asigna el valor del select a 'datos busqueda'
+    datosBusqueda.marca = e.target.value;
+
+    filtrarAuto();
+});
+
+year.addEventListener('change', e => {
+    // Asigna el valor del select a 'datos busqueda'
+    datosBusqueda.year = e.target.value;
+
+    filtrarAuto();
+});
+
+minimo.addEventListener('change', e => {
+    // Asigna el valor del select a 'datos busqueda'
+    datosBusqueda.minimo = e.target.value;
+
+    filtrarAuto();
+});
+
+maximo.addEventListener('change', e => {
+    // Asigna el valor del select a 'datos busqueda'
+    datosBusqueda.maximo = e.target.value;
+
+    filtrarAuto();
+});
+
+puertas.addEventListener('change', e => {
+    // Asigna el valor del select a 'datos busqueda'
+    datosBusqueda.puertas = e.target.value;
+
+    filtrarAuto();
+});
+
+transmision.addEventListener('change', e => {
+    // Asigna el valor del select a 'datos busqueda'
+    datosBusqueda.transmision = e.target.value;
+
+    filtrarAuto();
+});
+
+color.addEventListener('change', e => {
+    // Asigna el valor del select a 'datos busqueda'
+    datosBusqueda.color = e.target.value;
+
+    filtrarAuto();
 });
 
 
@@ -41,3 +114,37 @@ function limpiarHTML() {
         resultado.removeChild(resultado.firstChild);
     }
 }
+
+
+function filtrarAuto() {
+    console.log(datosBusqueda);
+};
+
+
+function llenarSelectYear() {
+    /* 
+     ? Queremos obtener los 10 ultimos años en base al año actual. 
+     ? ejemplo: si estamos en 2020 el año mínimo será 2011:
+     * <select>
+     *    <option>2020</option>
+     *    <option>2019</option>
+     *    <option>2018</option>
+     *    ...
+     *    <option>2011</option>
+     * </select>
+    */
+
+    // Obtener Año actual
+    const max = new Date().getFullYear();
+    const min = max - 10;
+
+    // Generar contenido del select 'year'
+    for( let i = max; i > min; i-- ) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+
+        // Agrega las opciones de año al select
+        year.appendChild(opcion);
+    };
+};
